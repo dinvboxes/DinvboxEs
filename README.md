7aaf7293d157ec2c7d1416d4083022bedd760133
dinvboxes <hola@dinvbox.es>
2026-04-19 23:08:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dinvbox website
---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    // Enable Next.js image optimization
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src 'self' data: https:; object-src 'none';"
+          },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" }
+        ]
+      }
+    ]
+  },
+}
+
+export default nextConfig
